c657a351b5d1c37a04bcc24aa0726a35300687ad
Susanna Carey <susannaopal@gmail.com>
2022-01-11 22:48:15 -0500

    Build out skeleton comp, work on CSS, and pseudocode future html ielements

--- src/scripts.js
+++ src/scripts.js
@@ -7,6 +7,10 @@ import './css/base.scss';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 // import './images/turing-logo.png'
 import './images/hotel-img.png'
+import './images/keys-icon.png'
+
+
+
 
 
 
