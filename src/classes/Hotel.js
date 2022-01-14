c233e8d91660529429afa34381899a57217e1381
Susanna Carey <susannaopal@gmail.com>
2022-01-13 20:24:55 -0500

    Add button for logout and fix hotel issue

--- src/classes/Hotel.js
+++ src/classes/Hotel.js
@@ -1,5 +1,9 @@
+import Customer from "./Customer"
 
 class Hotel {
+
+  //NEED TO FIX THE CONSTRUCTOR FOR STUFF THAT IS 
+  // ROOM OR BOOKING DATA BASED ON HOW IT IS BEING IMPORTED VIA THE FETCH CALLS (0-customerData, 2-roomData, 3-bookingData)
   constructor(currentCustomerData, roomData, bookingData) {  
     // this.hotels = hotelData;
     this.rooms = roomData;
