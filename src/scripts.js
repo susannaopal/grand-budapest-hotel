ec00e305e4eb739033af6f43ed909503061a718e
Susanna Carey <susannaopal@gmail.com>
2022-01-16 09:25:56 -0500

    Fix hiding issue

--- src/scripts.js
+++ src/scripts.js
@@ -67,8 +67,8 @@ const findFilteredByTagsRooms = (event) => {
   domUpdates.displayAllAvailableRooms(hotel.roomsByTag);
 };
 
-const returnToHomePage = (event) => {
-  event.preventDefault
+const returnToHomePage = () => {
+  // event.preventDefault;
   domUpdates.removeHidden([customerBookingsView]);
   domUpdates.addHidden([availRoomsViews]);
 };
